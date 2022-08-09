e2db1872d8e1e86aaa64970444b7966798bb57db
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-08-09 12:07:44 -0300

    Atualizar update com modificacoes do christian

--- app/src/main/java/br/com/zup/projetofinalzup/ui/home/view/HomeActivity.kt
+++ app/src/main/java/br/com/zup/projetofinalzup/ui/home/view/HomeActivity.kt
@@ -1,4 +1,4 @@
-package br.com.zup.projetofinalzup.ui.home
+package br.com.zup.projetofinalzup.ui.home.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -6,7 +6,7 @@ import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import br.com.zup.projetofinalzup.R
 import br.com.zup.projetofinalzup.databinding.ActivityHomeBinding
-import br.com.zup.projetofinalzup.ui.cart.view.CarttFragment
+import br.com.zup.projetofinalzup.ui.cart.view.CartFragment
 import br.com.zup.projetofinalzup.ui.favoritelist.view.FavoriteFragment
 import br.com.zup.projetofinalzup.ui.menu.view.MenuFragment
 
@@ -30,19 +30,16 @@ class HomeActivity : AppCompatActivity() {
             when(it.itemId){
                 R.id.Menu -> replaceFragment(MenuFragment())
                 R.id.Favoritos -> replaceFragment(FavoriteFragment())
-                R.id.Carrinho -> replaceFragment(CarttFragment())
+                R.id.Carrinho -> replaceFragment(CartFragment())
 
                 else -> {
-
                 }
             }
             true
         }
     }
 
-
     private fun replaceFragment(fragment: Fragment){
-
         val fragmentManager = supportFragmentManager
         val fragmentTransaction = fragmentManager.beginTransaction()
         fragmentTransaction.replace(R.id.frame_layout,fragment)
