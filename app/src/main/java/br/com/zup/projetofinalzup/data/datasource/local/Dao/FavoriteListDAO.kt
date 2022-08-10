e92e7dc74b312ec966add4424be8f05b812e407d
Adriano-konig <konigadri87@hotmail.comgit config --global user.email konigadri87@hotmail.com>
2022-08-10 15:24:37 -0300

    Implementar fun update

--- app/src/main/java/br/com/zup/projetofinalzup/data/datasource/local/Dao/FavoriteListDAO.kt
+++ app/src/main/java/br/com/zup/projetofinalzup/data/datasource/local/Dao/FavoriteListDAO.kt
@@ -1,8 +1,12 @@
 package br.com.zup.projetofinalzup.data.datasource.local.Dao
 
 import androidx.room.*
+import br.com.zup.projetofinalzup.data.model.CardapioResult
 
 @Dao
 interface FavoriteListDAO {
+
+    @Update(onConflict = OnConflictStrategy.IGNORE)
+    fun updateCardapioFavorite(cardapio: CardapioResult)
 
 }
