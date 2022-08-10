fa44b796cab25279d823e2c6c50bf1c0bc915781
Adriano-konig <konigadri87@hotmail.comgit config --global user.email konigadri87@hotmail.com>
2022-08-10 00:30:32 -0300

    Implementar fun favorito e disfavorito

--- app/src/main/java/br/com/zup/projetofinalzup/ui/detail/view/DetailFragment.kt
+++ app/src/main/java/br/com/zup/projetofinalzup/ui/detail/view/DetailFragment.kt
@@ -5,10 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import br.com.zup.projetofinalzup.R
+import br.com.zup.projetofinalzup.databinding.FragmentDetailBinding
+import br.com.zup.projetofinalzup.ui.favoritelist.viewmodel.FavoriteListViewModel
 
 
 class DetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -16,5 +23,12 @@ class DetailFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
+
+
+    private fun clickfavorito(){
+        //implementar o toast favorito e disfavorito
+    }
+
+
 
 }
