4afbc7b6a4d13a783b01d06bea95f4ca8dadca5e
Adriano-konig <konigadri87@hotmail.comgit config --global user.email konigadri87@hotmail.com>
2022-08-10 21:38:41 -0300

    Implementar fun update cardapio favorite

--- app/src/main/java/br/com/zup/projetofinalzup/ui/menu/viewmodel/MenuViewModel.kt
+++ app/src/main/java/br/com/zup/projetofinalzup/ui/menu/viewmodel/MenuViewModel.kt
@@ -1,4 +1,35 @@
 package br.com.zup.projetofinalzup.ui.menu.viewmodel
 
-class MenuViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.zup.projetofinalzup.data.model.CardapioResult
+import br.com.zup.projetofinalzup.domain.singleliveevent.SingleLiveEvent
+import br.com.zup.projetofinalzup.domain.usecase.DishesUseCase
+import br.com.zup.projetofinalzup.ui.viewstate.ViewState
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class MenuViewModel(application: Application) : AndroidViewModel(application){
+
+    private val dishesUseCase = DishesUseCase(application)
+    val cardapioFavoritedState = SingleLiveEvent<ViewState<CardapioResult>>()
+
+
+    fun updateCardapioFavorited(cardapioResult: CardapioResult) {
+        viewModelScope.launch {
+            try {
+                val response = withContext(Dispatchers.IO) {
+                    dishesUseCase.updateCardapioFavorite(cardapioResult)
+                }
+                cardapioFavoritedState.value = response
+            } catch (ex: Exception) {
+                cardapioFavoritedState.value =
+                    ViewState.Error(Throwable("Não foi possível atualizar o filme!"))
+            }
+        }
+    }
+
 }
