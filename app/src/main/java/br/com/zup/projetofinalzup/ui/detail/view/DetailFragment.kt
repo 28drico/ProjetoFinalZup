3a5dcdc90c5ddc0c0c54caeb48ac5dcaf4cee45c
Adriano-konig <konigadri87@hotmail.comgit config --global user.email konigadri87@hotmail.com>
2022-08-10 22:30:24 -0300

    Implementar fun toast favorite e disfavorite

--- app/src/main/java/br/com/zup/projetofinalzup/ui/detail/view/DetailFragment.kt
+++ app/src/main/java/br/com/zup/projetofinalzup/ui/detail/view/DetailFragment.kt
@@ -6,15 +6,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import br.com.zup.projetofinalzup.R
+import br.com.zup.projetofinalzup.data.model.CardapioResult
 import br.com.zup.projetofinalzup.databinding.FragmentDetailBinding
-import br.com.zup.projetofinalzup.ui.favoritelist.viewmodel.FavoriteListViewModel
+import br.com.zup.projetofinalzup.ui.DESFAVORITADO
+import br.com.zup.projetofinalzup.ui.FAVORITADO_SUCESSO
+import br.com.zup.projetofinalzup.ui.menu.viewmodel.MenuViewModel
 
 
 class DetailFragment : Fragment() {
 
     private lateinit var binding: FragmentDetailBinding
+
+    private val viewModel: MenuViewModel by lazy {
+        ViewModelProvider(this)[MenuViewModel::class.java]
+    }
+
+
 
 
     override fun onCreateView(
@@ -25,15 +35,25 @@ class DetailFragment : Fragment() {
     }
 
 
-    private fun clickfavorito(){
-        //implementar o toast favorito e disfavorito
+    private fun clickfavorito(cardapio: CardapioResult){
+        binding.ivFavorite.setOnClickListener {
+            cardapio.isFavorite = !cardapio.isFavorite
+//            updateFavorito(cardapio)
+
+
+            if (cardapio.isFavorite){
+                Toast.makeText(
+                    context,
+                    FAVORITADO_SUCESSO,
+                    Toast.LENGTH_LONG).show()
+            }else{
+                Toast.makeText(
+                    context,
+                    DESFAVORITADO,
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+        }
     }
-
-    private fun statusfavorito(){
-        //implementar codigo status favorito e disfavorito
-    }
-
-
-
 
 }
