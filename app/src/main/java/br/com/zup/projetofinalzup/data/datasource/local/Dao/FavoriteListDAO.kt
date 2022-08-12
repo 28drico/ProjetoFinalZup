ca6675ccb65afd05a0720f7a433186184b64f1fe
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-08-12 13:12:39 -0300

    Alterar estrutura de database

--- app/src/main/java/br/com/zup/projetofinalzup/data/datasource/local/Dao/FavoriteListDAO.kt
+++ app/src/main/java/br/com/zup/projetofinalzup/data/datasource/local/Dao/FavoriteListDAO.kt
@@ -1,19 +1,15 @@
-package br.com.zup.projetofinalzup.data.datasource.local.Dao
+package br.com.zup.projetofinalzup.data.datasource.local.dao
 
+import androidx.lifecycle.LiveData
 import androidx.room.*
 import br.com.zup.projetofinalzup.data.datasource.model.MenuItem
 
 @Dao
 interface FavoriteListDAO {
-    @Query("SELECT * FROM menu WHERE isFavorite = 1")
-    fun getFavoritedItems():List<MenuItem>
 
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    fun updateFavoritedItems(item: MenuItem)
+    @Query("Select * From item WHERE isFavorite = 1")
+    fun getFavoritedList(): LiveData<List<MenuItem>>
 
-    @Query("SELECT * FROM menu ORDER BY name ASC")
-    fun getMenu():List<MenuItem>
-
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertList(characters: List<MenuItem>)
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertFavoriteItem(item: List<MenuItem>)
 }
