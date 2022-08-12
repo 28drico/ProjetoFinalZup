a727f20c5ab8acb2d1bd62986efd55eb2bb0fd56
Adriano-konig <konigadri87@hotmail.comgit config --global user.email konigadri87@hotmail.com>
2022-08-12 19:23:05 -0300

    Implementar conexão dos favorito no banco de dados

--- app/src/main/java/br/com/zup/projetofinalzup/domain/repository/Repository.kt
+++ app/src/main/java/br/com/zup/projetofinalzup/domain/repository/Repository.kt
@@ -1,11 +1,14 @@
 package br.com.zup.projetofinalzup.domain.repository
 
+import br.com.zup.projetofinalzup.data.datasource.local.AppApplication
+import br.com.zup.projetofinalzup.data.datasource.model.MenuItem
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
-object Repository {
+object Repository{
+    private val dao = AppApplication.getdatabase().favoriteListDAO()
 
     const val URL_BASE = "https://fun-points-api.herokuapp.com"
 
@@ -15,5 +18,11 @@ object Repository {
         URL_BASE).build().create(
         MenuItemAPI::class.java)
     }
+
+    suspend fun insertFavoriteItem(item: MenuItem){
+        dao.insertFavoriteItem(item)
+    }
+
+    suspend fun getFavoritedList():List<MenuItem> = dao.getFavoritedList()
 
 }
