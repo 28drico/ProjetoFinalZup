780b9cdae34d754d1b9d7af508c3952ed2eb7551
oliveiranatalia <natalia.oliveira@zup.com.br>
2022-08-12 14:39:58 -0300

    Limpar use case

--- app/src/main/java/br/com/zup/projetofinalzup/domain/usecase/DishesUseCase.kt
+++ app/src/main/java/br/com/zup/projetofinalzup/domain/usecase/DishesUseCase.kt
@@ -1,50 +1,6 @@
 package br.com.zup.projetofinalzup.domain.usecase
-/**
-import android.app.Application
-import br.com.zup.projetofinalzup.data.datasource.local.FavoriteListDatabase
-import br.com.zup.projetofinalzup.data.datasource.model.MenuItem
-import br.com.zup.projetofinalzup.domain.repository.Repository
-import br.com.zup.projetofinalzup.ui.ERROR
-import br.com.zup.projetofinalzup.ui.viewstate.ViewState
 
-class DishesUseCase(application: Application) {
-    private val dao = FavoriteListDatabase.getDatabase(application).favoriteListDAO()
-    private val repository = Repository(dao)
+import br.com.zup.projetofinalzup.data.datasource.local.database.FavoriteListDatabase
 
-    suspend fun getMenuAPI(): ViewState<List<MenuItem>> {
-        return try{
-            val response = repository.getMenuAPI()
-            repository.insertDatabaseList(response.menu)
-            ViewState.Success(response.menu)
-        }catch(e:Exception){
-            getLocalList()
-        }
+class DishesUseCase() {
     }
-
-    suspend fun getFavoritedList():ViewState<List<MenuItem>>{
-        return try{
-            val favoritedItems = repository.getFavoritedList()
-            ViewState.Success(favoritedItems)
-        }catch(e:Exception){
-            ViewState.Error(Exception(ERROR))
-        }
-    }
-
-    suspend fun updateFavoritedList(menu:MenuItem):ViewState<MenuItem>{
-        return try{
-            repository.updateFavoritedList(menu)
-            ViewState.Success(menu)
-        }catch(e:Exception){
-            ViewState.Error(Exception(ERROR))
-        }
-    }
-
-    suspend fun getLocalList():ViewState<List<MenuItem>>{
-        return try{
-            val menu = repository.getLocalList()
-            ViewState.Success(menu)
-        }catch(e:Exception){
-            ViewState.Error(Exception(ERROR))
-        }
-    }
-}**/
