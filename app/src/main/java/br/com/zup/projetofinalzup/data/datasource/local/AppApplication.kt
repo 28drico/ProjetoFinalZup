bc0c5f7b8096ff2f132c5848b2495f9a543d2349
Christian de Abreu Cardoso <christian.cardoso@zup.com.br>
2022-08-17 18:27:07 -0300

    commits fav

--- app/src/main/java/br/com/zup/projetofinalzup/data/datasource/local/AppApplication.kt
+++ app/src/main/java/br/com/zup/projetofinalzup/data/datasource/local/AppApplication.kt
@@ -11,7 +11,7 @@ class AppApplication: Application() {
             applicationContext,
             FavoriteListDatabase::class.java, "database-character"
         )
-            .fallbackToDestructiveMigration()
+            .fallbackToDestructiveMigration().allowMainThreadQueries()
             .build()
 
     }
--- app/src/main/java/br/com/zup/projetofinalzup/ui/detail/view/DetailFragment.kt
+++ app/src/main/java/br/com/zup/projetofinalzup/ui/detail/view/DetailFragment.kt
@@ -25,16 +25,18 @@ class DetailFragment : Fragment() {
     ): View {
         binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
         factory = DetailViewModel.DetailModelFactory()
-        viewModel = ViewModelProvider(this,factory).get(DetailViewModel::class.java)
+        viewModel = ViewModelProvider(this, factory).get(DetailViewModel::class.java)
         return binding.root
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         dataRecovery()
     }
-    private fun dataRecovery(){
+
+    private fun dataRecovery() {
         val item = arguments?.getParcelable<MenuItem>("ITEM_KEY")
-        item?.let{
+        item?.let {
             Picasso.get().load(it.urlImageProduct).into(binding.ivItemDetail)
             binding.tvItemTitle.text = it.name
             binding.tvItemDescription.text = it.description
@@ -42,38 +44,65 @@ class DetailFragment : Fragment() {
             binding.tvItemPrice.text = value
             updateColor(item)
         }
-        binding.tvCartAdd.setOnClickListener{
+        binding.tvCartAdd.setOnClickListener {
             if (item != null) {
                 addItemCart(item)
             }
         }
-        binding.ivFavorite.setOnClickListener{
+        binding.ivFavorite.setOnClickListener {
             if (item != null) {
                 item.isFavorite = !item.isFavorite
-                updateColor(item)
-                favoriteItem(item)
                 viewModel.updateFavoritedList(item)
-                binding.ivFavorite.setImageDrawable(ContextCompat.getDrawable
-                    (binding.root.context,if (item.isFavorite) R.drawable.fav_icon else R.drawable.icon_heart))
+                favoriteItem(item)
+                verifyIconFavorite(item.isFavorite)
+
             }
         }
     }
 
-    private fun addItemCart(item:MenuItem){
+    private fun addItemCart(item: MenuItem) {
         viewModel.sendItemToCart(item)
-        Toast.makeText(context,R.string.item_add,Toast.LENGTH_SHORT).show()
+        Toast.makeText(context, R.string.item_add, Toast.LENGTH_SHORT).show()
     }
 
-    private fun updateColor(item: MenuItem){
-        binding.ivFavorite.setImageDrawable(ContextCompat.getDrawable
-            (binding.root.context,if (item.isFavorite) R.drawable.fav_icon else R.drawable.icon_heart))
+    private fun updateColor(item: MenuItem) {
+        var isFavorite = false
+        var listFavorite = viewModel.favoriteList
+        listFavorite?.forEach {
+            if (it.name == item.name) {
+                isFavorite = true
+            }
+        }
+        verifyIconFavorite(isFavorite)
     }
 
-    fun favoriteItem(item:MenuItem){
-        if (item.isFavorite){
-            Toast.makeText(context,"${item.name} ${getString(R.string.item_fav)}",Toast.LENGTH_SHORT).show()
-        }else{
-            Toast.makeText(context,"${item.name} ${getString(R.string.item_disfav)}",Toast.LENGTH_SHORT).show()
+    private fun verifyIconFavorite(isFavorite: Boolean) {
+        binding.ivFavorite.setImageDrawable(
+            ContextCompat.getDrawable
+                (
+                binding.root.context,
+                if (isFavorite) {
+                    R.drawable.fav_icon
+                } else {
+                    R.drawable.icon_heart
+                }
+            )
+        )
+    }
+
+    fun favoriteItem(item: MenuItem) {
+        if (item.isFavorite) {
+            Toast.makeText(
+                context,
+                "${item.name} ${getString(R.string.item_fav)}",
+                Toast.LENGTH_SHORT
+            ).show()
+        } else {
+            Toast.makeText(
+                context,
+                "${item.name} ${getString(R.string.item_disfav)}",
+                Toast.LENGTH_SHORT
+            ).show()
         }
     }
 }
